5760e8e028e40c9ea8c8096afb5a6157f24db84e
Emirhan Saç <emirhansac5353@gmail.com>
2022-04-08 03:46:02 +0300

    issues fixed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,8 +8,16 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] float speed = 5f;
     [SerializeField] bool isPlaying;
+    bool isTouched = false;
+
+    [Header("Jump")]
+    [SerializeField] float yForce = 5f;
+    [SerializeField] float zForce = 2f;
 
     [Header("Score")]
+    [SerializeField] int redScore;
+    [SerializeField] int orangeScore;
+    [SerializeField] int goldScore;
     [SerializeField] TextMeshProUGUI scoreText;
     [SerializeField] int score;
 
@@ -18,13 +26,15 @@ public class PlayerController : MonoBehaviour
     [SerializeField] static float globalGravity = -9.81f;
 
     Vector3 runTowards;
-    [HideInInspector] public Animator animator;
+    Animator animator;
     Rigidbody rb;
+    TargetRamps targetRamps;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
         animator = GetComponent<Animator>();
+        targetRamps = FindObjectOfType<TargetRamps>();
     }
 
     void Start()
@@ -57,12 +67,50 @@ public class PlayerController : MonoBehaviour
         rb.useGravity = false;
     }
 
+    void Jump()
+    {
+        rb.velocity = new Vector3(0, yForce, zForce);
+        animator.SetTrigger("Jumping");
+    }
+
     private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.tag == "Platform" && isPlaying)
         {
             animator.SetTrigger("Running");
         }
+
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            if (other.gameObject.tag == "RedRound" && !isTouched)
+            {
+                Debug.Log("RED!");
+                isTouched = true;
+                Jump();
+                IncreaseScore(redScore);
+                ShowScore();
+                other.gameObject.GetComponent<MeshRenderer>().material.color = Color.white;
+            }
+            else if (other.gameObject.tag == "OrangeRound" && !isTouched)
+            {
+                Debug.Log("ORANGE!");
+                isTouched = true;
+                Jump();
+                IncreaseScore(orangeScore);
+                ShowScore();
+                other.gameObject.GetComponent<MeshRenderer>().material.color = Color.white;
+            }
+            else if (other.gameObject.tag == "GoldRound" && !isTouched)
+            {
+                Debug.Log("GOLD!");
+                isTouched = true;
+                Jump();
+                IncreaseScore(goldScore);
+                ShowScore();
+                other.gameObject.GetComponent<MeshRenderer>().material.color = Color.white;
+            }
+        }
+        isTouched = false;
     }
 
     public void IncreaseScore(int value)
--- Assets/Scripts/TargetRamps.cs
+++ Assets/Scripts/TargetRamps.cs
@@ -4,61 +4,12 @@ using UnityEngine;
 
 public class TargetRamps : MonoBehaviour
 {
-    [Header("Jump")]
-    [SerializeField] float yForce = 5f;
-    [SerializeField] float zForce = 2f;
-
-    [Header("Rounds")]
-    [SerializeField] bool isRed;
-    [SerializeField] bool isOrange;
-    [SerializeField] bool isGold;
-
-    [Header("Score")]
-    [SerializeField] int redScore;
-    [SerializeField] int orangeScore;
-    [SerializeField] int goldScore;
-
     PlayerController player;
-    Material material;
+    [HideInInspector] public Material material;
 
     void Awake()
     {
         player = FindObjectOfType<PlayerController>();
         material = GetComponent<Renderer>().material;
     }
-
-    void PlayerJump(Collision value)
-    {
-        value.rigidbody.velocity = new Vector3(0, yForce, zForce);
-        player.animator.SetTrigger("Jumping");
-    }
-
-    private void OnCollisionEnter(Collision other)
-    {
-        if (other.gameObject.tag == "Player" && Input.GetKey(KeyCode.Mouse0))
-        {
-            if (isRed)
-            {
-                PlayerJump(other);
-                player.IncreaseScore(redScore);
-                player.ShowScore();
-                material.color = Color.white;
-            }
-            else if (isOrange)
-            {
-                PlayerJump(other);
-                player.IncreaseScore(orangeScore);
-                player.ShowScore();
-                material.color = Color.white;
-            }
-            else if (isGold)
-            {
-                PlayerJump(other);
-                player.IncreaseScore(goldScore);
-                player.ShowScore();
-                material.color = Color.white;
-            }
-        }
-    }
-
 }
